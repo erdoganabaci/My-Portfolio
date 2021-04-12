df98e08cd272168280ebc8a7734d17f00b65a3fa
erdoganabaci <erdoganabaci97@gmail.com>
2021-04-12 03:14:51 +0300

    update resumeLink

--- src/portfolio.js
+++ src/portfolio.js
@@ -15,7 +15,7 @@ const greeting = {
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing",
+    "https://drive.google.com/file/d/1eoVoJdSLPx1rzRlWDgS88c6RVpQROiwF/view?usp=sharing",
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
